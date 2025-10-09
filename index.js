e8e30f2273d6b23fde5786968b5cdec10580e8c2
laizaservinn <laizadservin@gmail.com>
2025-10-09 10:01:16 -0300

    criacao scrpit dev

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+import { JSDOM } from 'jsdom';
+
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+const document = dom.window.document;
+console.log("chegou document",document); 
+
 // Alternar tema claro/escuro
 const body = document.body;
 const themeToggle = document.getElementById("theme-toggle");
-
+console.log("chegou themeToggle",themeToggle);
 themeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  themeToggle.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
+  console.log('Tema alternado!');
+  // body.classList.toggle("dark");
+  // themeToggle.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
 });
 
 // Filtro de posts
